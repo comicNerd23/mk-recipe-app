93669ff224c2d9570b3c323b86c6ed304263ceb8
comicnerd23 <m23pal@gmail.com>
2021-12-22 08:39:28 +0530

    Initial Recipe Project Add

---
+++ src/main/java/com/moulik/mkrecipeapp/MkRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.moulik.mkrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moulik/mkrecipeapp/MkRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moulik.mkrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
