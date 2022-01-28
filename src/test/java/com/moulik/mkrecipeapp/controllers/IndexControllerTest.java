b3f08daaaac4e530acb5fb61a8490516d5f5a90b
comicnerd23 <m23pal@gmail.com>
2022-01-28 21:47:10 +0530

    Spring Integration Test Maven Configuration

--- src/test/java/com/moulik/mkrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/moulik/mkrecipeapp/controllers/IndexControllerTest.java
@@ -18,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 class IndexControllerTest {
 
