2d017d7a8f3aa70777c4f15090dd3a983ac03819
comicnerd23 <m23pal@gmail.com>
2022-09-19 23:47:52 +0200

    Create a recipe: Recipe Form to show/update recipe

--- src/main/java/com/moulik/mkrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/moulik/mkrecipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.moulik.mkrecipeapp.controllers;
 
+import com.moulik.mkrecipeapp.commands.RecipeCommand;
 import com.moulik.mkrecipeapp.domain.Recipe;
 import com.moulik.mkrecipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +24,17 @@ public class RecipeController {
         Recipe recipe = recipeService.findById(Long.valueOf(id));
         model.addAttribute("recipe", recipe);
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
