a6e4643062abe5cb8f348178513557a1ea526b08
comicnerd23 <m23pal@gmail.com>
2022-09-24 10:45:29 +0200

    Fixing recipe controller test

--- src/test/java/com/moulik/mkrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/moulik/mkrecipeapp/controllers/RecipeControllerTest.java
@@ -2,34 +2,31 @@ package com.moulik.mkrecipeapp.controllers;
 
 import com.moulik.mkrecipeapp.domain.Recipe;
 import com.moulik.mkrecipeapp.services.RecipeService;
-import com.moulik.mkrecipeapp.services.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class RecipeControllerTest {
     
     RecipeController recipeController;
 
     @Mock
     RecipeService recipeService;
-
-    @Mock
-    Model model;
     
     @BeforeEach
     void setUp() {
         //MockitoAnnotations.initMocks(this);
-        MockitoAnnotations.openMocks(this);
+        //MockitoAnnotations.openMocks(this);
         recipeController = new RecipeController(recipeService);
     }
 
@@ -40,7 +37,7 @@ public class RecipeControllerTest {
         when(recipeService.findById(anyLong())).thenReturn(recipe);
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/show/1"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/show"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("recipe/show"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
